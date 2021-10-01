7fc12f6884e6418317834064af2a38ea0695ee2e
JillKlatt <klatt.jill@gmail.com>
2021-10-01 00:30:35 -0400

    Add Question modal

---
+++ src/Questions/QuestionModal.js
@@ -0,0 +1,33 @@
+import './QuestionModal.css'
+import { useState } from 'react'
+import AnswerModal from './AnswerModal'
+
+
+function QuestionModal({question, answer, id, toggleModal, showModal}) {
+
+    const [showAnswer, setShowAnswer] = useState(false)
+
+    const closeModal = () => {
+        toggleModal()
+    }
+
+    const toggleAnswer = () => {
+        setShowAnswer(showAnswer => !showAnswer)
+    }
+
+    return (
+        <>
+        <div id={id} className="modal" style={{display: showModal ? "block" : "none"}}>
+            <div className='modal-content' style={{display: showModal ? "block" : "none"}}>
+                {showAnswer ? 
+                <AnswerModal answer={answer} showAnswer={showAnswer} toggleModal={toggleModal}/> :
+                null }
+                <p>{question.toUpperCase()}</p>
+                <span className='answer' onClick={toggleAnswer}>answer</span>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default QuestionModal
